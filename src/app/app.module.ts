4e3bab0ab039abad3ec45dd32180966a2e032f79
Mourad Samy <mouradsamyroushdy@gmail.com>
2021-02-26 17:47:17 +0200

    Add main layout including header and footer
    - Use Case: #1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BbUIModule } from './bb-ui/bb-ui.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BbUIModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
